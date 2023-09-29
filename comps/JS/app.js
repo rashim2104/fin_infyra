39aa0d0885d6990f65f1e8a2730e4947fe4d3ddb
Rashim <122736673+rashim2104@users.noreply.github.com>
2023-09-29 13:30:34 +0530

    Initial

--- comps/JS/app.js
+++ comps/JS/app.js
@@ -1,8 +1,6 @@
-document.querySelector('.hamburger').addEventListener('click', (e)=>{
-    document.querySelector('.nav-buttons-mobile').classList.toggle('hidden-nav')
-    let i=0;
-    document.querySelectorAll('.hamburger span').forEach((span)=>{ 
-      i+=1; 
-      span.classList.toggle(`close-${i}`);
-     })
-  })
+const menuToggle = document.querySelector('.menu-toggle');
+const navButtons = document.querySelector('.nav-buttons');
+
+menuToggle.addEventListener('click', () => {
+  navButtons.classList.toggle('nav-open');
+});
